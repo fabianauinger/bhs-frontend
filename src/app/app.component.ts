f902d0b2540b484179d217167a84fde372514e22
Fabian Auinger <fabian.auinger11@gmail.com>
2025-07-10 22:04:05 +0200

    implementing environment variable for environment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +11,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'bhs-frontend';
+  title = 'bhs-frontend - '+environment.envName;
 }
---
+++ src/environments/environment.test.ts
@@ -0,0 +1,6 @@
+// environment.test.ts
+export const environment = {
+    production: false,
+    envName: 'TEST'
+  };
+  
