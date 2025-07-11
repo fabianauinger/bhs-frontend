bd585b79a02cd40213efc9486a97e7edb8160e5b
Fabian Auinger <fabian.auinger11@gmail.com>
2025-07-11 14:16:34 +0200

    added version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { environment } from '../environments/environment';
+import { version } from '../version'; // Assuming version.ts is in the src directory
 
 @Component({
   selector: 'app-root',
@@ -13,4 +14,6 @@ import { environment } from '../environments/environment';
 export class AppComponent {
   title = 'bhs-frontend - ' + environment.envName;
   backendUrl = environment.backendUrl;
+  buildTime = version.buildTime; // Accessing the version from version.ts
+  commitHash = version.commitHash; // Accessing the version from version.ts 
 }
